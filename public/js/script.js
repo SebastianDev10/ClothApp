a13047fc4361fae8657e45846856673baea86840
SebastianDev10 <128023588+SebastianDev10@users.noreply.github.com>
2023-06-29 21:02:21 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,46 @@
-const form = document.querySelector("form");
-const emailInput = form.querySelector('input[name="email"]');
-const confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
-
-function isEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-}
-
-function arePasswordsSame(password, confirmedPassword) {
-    return password === confirmedPassword;
-}
-
-function markValidation(element, condition) {
-    !condition ? element.classList.add('no-valid') : element.classList.remove('no-valid');
-}
-
-function validateEmail() {
-    setTimeout(function () {
-            markValidation(emailInput, isEmail(emailInput.value));
-        },
-        1000
-    );
-}
-
-function validatePassword() {
-    setTimeout(function () {
-            const condition = arePasswordsSame(
-                confirmedPasswordInput.previousElementSibling.value,
-                confirmedPasswordInput.value
-            );
-            markValidation(confirmedPasswordInput, condition);
-        },
-        1000
-    );
-}
-
-emailInput.addEventListener('keyup', validateEmail);
-confirmedPasswordInput.addEventListener('keyup', validatePassword);
+class FormValidator {
+    constructor(form) {
+      this.form = form;
+      this.emailInput = form.querySelector('input[name="email"]');
+      this.confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
+    }
+  
+    init() {
+      this.emailInput.addEventListener('keyup', this.validateEmail.bind(this));
+      this.confirmedPasswordInput.addEventListener('keyup', this.validatePassword.bind(this));
+    }
+  
+    isEmail(email) {
+      return /\S+@\S+\.\S+/.test(email);
+    }
+  
+    arePasswordsSame(password, confirmedPassword) {
+      return password === confirmedPassword;
+    }
+  
+    markValidation(element, condition) {
+      !condition ? element.classList.add('no-valid') : element.classList.remove('no-valid');
+    }
+  
+    validateEmail() {
+      setTimeout(() => {
+        this.markValidation(this.emailInput, this.isEmail(this.emailInput.value));
+      }, 1000);
+    }
+  
+    validatePassword() {
+      setTimeout(() => {
+        const condition = this.arePasswordsSame(
+          this.confirmedPasswordInput.previousElementSibling.value,
+          this.confirmedPasswordInput.value
+        );
+        this.markValidation(this.confirmedPasswordInput, condition);
+      }, 1000);
+    }
+  }
+  
+  const form = document.querySelector("form");
+  
+  const formValidator = new FormValidator(form);
+  formValidator.init();
+  
