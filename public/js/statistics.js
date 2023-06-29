ec8f15a977d19eefb0ec12773ec2052b8dac8b76
SebastianDev10 <128023588+SebastianDev10@users.noreply.github.com>
2023-06-29 21:05:55 +0200

    Update statistics.js

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -1,28 +1,40 @@
-const likeButtons = document.querySelectorAll(".fa-heart");
-const dislikeButtons = document.querySelectorAll(".fa-minus-square");
 
-
-function giveLike() {
-    const likes = this;
-    const container = likes.parentElement.parentElement.parentElement;
-    const id = container.getAttribute("id");
-
-    fetch(`/like/${id}`)
-        .then(function () {
-            likes.innerHTML = parseInt(likes.innerHTML) + 1;
-        })
-}
-
-function giveDislike() {
-    const dislikes = this;
-    const container = dislikes.parentElement.parentElement.parentElement;
-    const id = container.getAttribute("id");
-
-    fetch(`/dislike/${id}`)
-        .then(function () {
-            dislikes.innerHTML = parseInt(dislikes.innerHTML) + 1;
-        })
-}
-
-likeButtons.forEach(button => button.addEventListener("click", giveLike));
-dislikeButtons.forEach(button => button.addEventListener("click", giveDislike));
+class LikeDislikeButtons {
+    constructor(likeButtons, dislikeButtons) {
+      this.likeButtons = likeButtons;
+      this.dislikeButtons = dislikeButtons;
+    }
+  
+    init() {
+      this.likeButtons.forEach(button => button.addEventListener("click", this.giveLike.bind(this)));
+      this.dislikeButtons.forEach(button => button.addEventListener("click", this.giveDislike.bind(this)));
+    }
+  
+    giveLike(event) {
+      const likes = event.target;
+      const container = likes.parentElement.parentElement.parentElement;
+      const id = container.getAttribute("id");
+  
+      fetch(`/like/${id}`)
+        .then(() => {
+          likes.innerHTML = parseInt(likes.innerHTML) + 1;
+        });
+    }
+  
+    giveDislike(event) {
+      const dislikes = event.target;
+      const container = dislikes.parentElement.parentElement.parentElement;
+      const id = container.getAttribute("id");
+  
+      fetch(`/dislike/${id}`)
+        .then(() => {
+          dislikes.innerHTML = parseInt(dislikes.innerHTML) + 1;
+        });
+    }
+  }
+  
+  const likeButtons = document.querySelectorAll(".fa-heart");
+  const dislikeButtons = document.querySelectorAll(".fa-minus-square");
+  
+  const likeDislike = new LikeDislikeButtons(likeButtons, dislikeButtons);
+  likeDislike.init();
